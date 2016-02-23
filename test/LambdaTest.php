b09d4cd447ce242c26354d3667acdc7f799d9470
Christian <christian@anysrc.net>
2016-02-23 13:34:26 +0100

    testing a test 4

--- test/LambdaTest.php
+++ test/LambdaTest.php
@@ -6,12 +6,12 @@ class Test extends PHPUnit_Framework_TestCase
     public function testScalar()
     {
         $s = new \PerrysLambda\ScalarProperty("Zähn € zahme Ziegen zögen zwei Zentner Zücker zum Zoö!");
-        $this->assertSame($s->startsWith("Ze"), true);
+        $this->assertSame($s->startsWith("Zä"), true);
     }
     
     public function testLambda()
     {
-        $data = json_decode(file_get_contents(__DIR__."/../examples/testdata.json"));
+        $data = json_decode(file_get_contents(__DIR__."/../examples/testdata.json"), true);
         $collection = \PerrysLambda\ArrayList::asObjectArray($data);
         $this->assertSame($collection->take(1)->length(), 1);
     }
